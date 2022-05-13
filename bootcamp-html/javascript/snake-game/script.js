eab43bf49099dbafab05c09c1615ff2c5b426385
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2022-05-12 22:22:55 -0300

    personalização da página do game

--- bootcamp-html/javascript/snake-game/script.js
+++ bootcamp-html/javascript/snake-game/script.js
@@ -13,7 +13,7 @@ let food = {
 }
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo usando x e y e a largura e altura setadas
 }
 
